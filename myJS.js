5403dc804321f2a861c8eb94cc709d8b34e2cbb2
sedwar581 <steven.edwards.tech@gmail.com>
2019-09-14 08:37:25 -0400

    First commit for the menu icon repo.  The transition happens by mouseover as a test

---
+++ myJS.js
@@ -0,0 +1,9 @@
+function transitionToX() {
+    let top_bar = document.getElementById("upper_bar");
+    let bottom_bar = document.getElementById("lower_bar");
+    let menu_icon = document.getElementById("toggle-menu");
+    
+    menu_icon.setAttribute("style", "opacity: 0.05");
+    top_bar.setAttribute("style", "transform: matrix(0.7, 0.8, -0.6, 0.6, 0, 20)");
+    bottom_bar.setAttribute("style", "transform: matrix(0.7, -0.8, 0.6, 0.6, 0, -25)");
+}
