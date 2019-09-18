839ec62c830788f75600b86b755b759361541f99
sedwar581 <steven.edwards.tech@gmail.com>
2019-09-18 08:24:55 -0400

    still working to find out why the onClick event doesn't work as I expect just yet

--- myJS.js
+++ myJS.js
@@ -1,8 +1,11 @@
+
 function transitionToX() {
     let top_bar = document.getElementById("upper_bar");
     let bottom_bar = document.getElementById("lower_bar");
     let menu_icon = document.getElementById("toggle-menu");
     
+    console.log("Hello World.");
+
     menu_icon.setAttribute("style", "opacity: 0.05");
     top_bar.setAttribute("style", "transform: matrix(0.7, 0.8, -0.6, 0.6, 0, 20)");
     bottom_bar.setAttribute("style", "transform: matrix(0.7, -0.8, 0.6, 0.6, 0, -25)");
