ee37511cff0489a5934ea9503eaa5cb0f9d4b4f7
sedwar581 <steven.edwards.tech@gmail.com>
2019-09-19 03:56:38 -0400

    modified the html, CSS and JS so that the icon and bars are the same size as what should be used on the health-e website

--- myJS.js
+++ myJS.js
@@ -1,23 +1,22 @@
-
 function transitionMenu() {
     let upper_bar = document.getElementById("upper_bar");
     let lower_bar = document.getElementById("lower_bar");
     let menu_icon = document.getElementById("toggle-menu");
-    let menu_button = document.getElementById("menu-anchor");
+    let menu_anchor = document.getElementById("menu-anchor");
 
-    console.log(menu_button.className);
+    console.log(menu_anchor.className);
 
-    if ( menu_button.className == "menu_closed" ) {
+    if ( menu_anchor.className == "menu_closed" ) {
         console.log(" opening the menu. ");
         menu_icon.setAttribute("style", "opacity: 0.05");
-        upper_bar.setAttribute("style", "transform: matrix(0.8, 0.8, -0.5, 0.8, 0, 16)");
-        lower_bar.setAttribute("style", "transform: matrix(0.8, -0.8, 0.5, 0.8, 0, -15)");
-        menu_button.setAttribute("class", "menu_opened");
-    } else if ( menu_button.className == "menu_opened" ) {
+        upper_bar.setAttribute("style", "transform: matrix(0.8, 0.8, -0.5, 0.8, 0, 10)");
+        lower_bar.setAttribute("style", "transform: matrix(0.8, -0.8, 0.5, 0.8, 0, -9)");
+        menu_anchor.setAttribute("class", "menu_opened");
+    } else if ( menu_anchor.className == "menu_opened" ) {
         console.log(" closing the menu. ");
-        upper_bar.setAttribute("style", "transform: matrix(0.8, 0, 0, 0.8, 0, 0)");
-        lower_bar.setAttribute("style", "transform: matrix(0.8, 0, 0, 0.8, 0, 0)");
-        menu_button.setAttribute("class", "menu_closed");
+        upper_bar.setAttribute("style", "transform: matrix(1, 0, 0, 1, 0, 0)");
+        lower_bar.setAttribute("style", "transform: matrix(1, 0, 0, 1, 0, 0)");
+        menu_anchor.setAttribute("class", "menu_closed");
         menu_icon.setAttribute("style", "opacity: 1");
     }
 }
